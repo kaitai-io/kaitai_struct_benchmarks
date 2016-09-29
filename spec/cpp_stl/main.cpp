3cefca863dd992d2ae877f721bcac994a8c3f663
Mikhail Yakshin <greycat@altlinux.org>
2016-09-29 20:08:29 +0300

    C++: rewrote main to allow multiple iterations and do averaging

--- spec/cpp_stl/main.cpp
+++ spec/cpp_stl/main.cpp
@@ -23,16 +23,45 @@ benchmark_case benchmarks[] = {
 
 struct timeval t1, t2, t3;
 
-void report_time(std::string caption, struct timeval timeStart, struct timeval timeEnd) {
-    std::cout << caption << " : " << ((timeEnd.tv_sec - timeStart.tv_sec) * 1000000 + timeEnd.tv_usec - timeStart.tv_usec) << std::endl;
+long delta_time(struct timeval timeStart, struct timeval timeEnd) {
+    return (timeEnd.tv_sec - timeStart.tv_sec) * 1000000 + timeEnd.tv_usec - timeStart.tv_usec;
+}
+
+void perform_benchmark(void (*main_test_func)(), int it_count) {
+    long d1_sum = 0;
+    long d2_sum = 0;
+    long d1_sum2 = 0;
+    long d2_sum2 = 0;
+
+    std::cout << std::fixed;
+
+    for (int i = 0; i < it_count; i++) {
+        main_test_func();
+        long d1 = delta_time(t1, t2);
+        long d2 = delta_time(t2, t3);
+        std::cout << "load: " << d1 << std::endl;
+        std::cout << "calc: " << d2 << std::endl;
+        d1_sum += d1;
+        d2_sum += d2;
+        d1_sum2 += d1 * d1;
+        d2_sum2 += d2 * d2;
+        std::cout << "d2_sum: " << d2_sum << std::endl;
+    }
+
+    double d1_avg = (double) d1_sum / 1000000.0 / it_count;
+    double d2_avg = (double) d2_sum / 1000000.0 / it_count;
+
+    std::cout << "Ran " << it_count << " iterations" << std::endl;
+    std::cout << "load: " << d1_avg << std::endl;
+    std::cout << "calc: " << d2_avg << std::endl;
 }
 
 int main(int argc, char** argv) {
     int bm_count = sizeof(benchmarks) / sizeof(benchmark_case);
 
-    if (argc != 2) {
+    if (argc != 3) {
         std::cout <<
-            "Usage: " << argv[0] << " <benchmark>" << std::endl <<
+            "Usage: " << argv[0] << " <benchmark> <iterations>" << std::endl <<
             std::endl <<
             "Available benchmarks:" << std::endl;
 
@@ -55,10 +84,9 @@ int main(int argc, char** argv) {
         return 2;
     }
 
-    main_test_func();
+    int it_count = std::atoi(argv[2]);
 
-    report_time("load", t1, t2);
-    report_time("calc", t2, t3);
+    perform_benchmark(main_test_func, it_count);
 
     return 0;
 }
