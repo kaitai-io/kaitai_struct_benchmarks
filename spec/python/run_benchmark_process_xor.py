106507f2c0ef4f2c52927725061c6c376857a100
Arkadiusz Bulski <arek.bulski@gmail.com>
2018-01-21 17:11:16 +0100

    benchmarks produce deterministic data, and clean up
    * gen_benchmark_process_xor needs to be deterministic, for deterministic time results
    
    * gen_* scripts cleanup
    
    * run_* preliminary cleanup
    
    * fixed scripts
    
    * printing is rich, not just numbers without description
    
    * further cleanup

--- spec/python/run_benchmark_process_xor.py
+++ spec/python/run_benchmark_process_xor.py
@@ -4,20 +4,18 @@ import os
 from timeit import default_timer as timer
 from benchmark_process_xor import BenchmarkProcessXor
 
-maxnum = 0
-
 t1 = timer()
 
 r = BenchmarkProcessXor.from_file("%s/benchmark_process_xor.dat" % os.environ['DATA_DIR'])
 
 t2 = timer()
 
+sum = 0
 for chunk in r.chunks:
-    tm = max(chunk.body.numbers)
-    maxnum = max(tm, maxnum)
+    for n in chunk.body.numbers:
+        sum += n
 
 t3 = timer()
 
-print("max = %d" % maxnum)
-print(t2 - t1)
-print(t3 - t2)
+print("parsing: %s seconds" % (t2-t1,))
+print("processing: %s seconds" % (t3-t2,))
--- spec/python/run_ext2.py
+++ spec/python/run_ext2.py
@@ -4,6 +4,12 @@ import os
 from timeit import default_timer as timer
 from ext2 import Ext2
 
+t1 = timer()
+
+r = Ext2.from_file("%s/ext2.dat" % os.environ['DATA_DIR'])
+
+t2 = timer()
+
 def calc_dir(dir):
     sum = 0
     for entry in dir.entries:
@@ -13,19 +19,11 @@ def calc_dir(dir):
             sum += calc_dir(entry.inode.as_dir)
         elif entry.file_type == Ext2.DirEntry.FileType.reg_file:
             sum += entry.inode.size
-
     return sum
 
-t1 = timer()
-
-r = Ext2.from_file("%s/ext2.dat" % os.environ['DATA_DIR'])
-
-t2 = timer()
-
-sum = calc_dir(r.root_dir)
+calc_dir(r.root_dir)
 
 t3 = timer()
 
-print("sum = %d" % calc_dir(r.root_dir))
-print(t2 - t1)
-print(t3 - t2)
+print("parsing: %s seconds" % (t2-t1,))
+print("processing: %s seconds" % (t3-t2,))
